dffa2ef95d9190b707222d509d93ed43aa367a0c
Nefo91 <88011585+Nefo91@users.noreply.github.com>
2022-05-01 23:00:30 -0600

    commit 1

---
+++ src/modelo/main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author NefiM
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frm_tabla_hash frm = new frm_tabla_hash();
+        frm.show();
+    }
+    
+}
